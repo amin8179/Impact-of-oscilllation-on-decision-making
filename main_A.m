d87ab2d681b31c4d5cc5187b6967a40922839b2d
Amin Azimi <93032269+amin8179@users.noreply.github.com>
2024-12-22 17:05:36 +0000

    main_A.m

--- main_A.m
+++ main_A.m
@@ -18,7 +18,7 @@ phi = 0:pi/4:pi;               % Phase differences for the sine wave inputs
 for i=1:length(a_values1)
 
 % Simulation of network A
-[acc(i),decision_reaction_times,error_reaction_times,decision_trial_indices,error_trial_indices,non_decision_trial_indices{i},avg_decision_rts(i),avg_error_rts(i),x2_thr{i},x17_thr{i},x2_thre{i},x17_thre{i},stc_rtc(i),std_error_rts(i), o1_e1,o2_e1,input_1,input_2]=net_AS(num_trials,fs,decision_boundary,a_values1(i),0.07,12,phi,a_values2(i));
+[acc(i),decision_reaction_times,error_reaction_times,decision_trial_indices,error_trial_indices,non_decision_trial_indices{i},avg_decision_rts(i),avg_error_rts(i),x2_thr{i},x17_thr{i},x2_thre{i},x17_thre{i},stc_rtc(i),std_error_rts(i), o1_e1,o2_e1,input_1,input_2]=fsm_A(num_trials,fs,decision_boundary,a_values1(i),0.07,12,phi,a_values2(i));
 
 end
 
