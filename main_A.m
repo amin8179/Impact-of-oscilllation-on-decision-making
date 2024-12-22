efe89acd3fa253c4744806c56b2a714cefa91f85
Amin Azimi <93032269+amin8179@users.noreply.github.com>
2024-12-22 16:53:34 +0000

    main_A.m

--- main_A.m
+++ main_A.m
@@ -1,19 +1,25 @@
 % This script simulates network A over multiple trials, varying evidence qualities and phase differences.
 
 clear all
+num_trials=3000;   % Number of trials
+fs=1000;           % Sampling frequency
 
-num_trials = 3000;             % Number of trials
-fs = 1000;                     % Sampling frequency
-decision_boundary = 0.005;     % Decision threshold
-a_values = 0.1:0.025:0.35;     % Evidence qualities (input amplitudes)
+decision_boundary=0.026;  % Decision threshold
+
+a_values1 = [0.6:0.1/10:0.8];    % Amplitude of stimulus inputs to column 1
+a_values2 = [0.6:(-0.1)/10:0.4]; % Amplitude of stimulus inputs to column 2
+
+ev=(a_values1-a_values2)./(a_values1+a_values2);  % Evidence qualities (input amplitudes)
+
 phi = 0:pi/4:pi;               % Phase differences for the sine wave inputs
 
 % Loop over different evidence qualities and phase differences
-for i = 1:length(a_values)
-    for j = 1:length(phi)
-        % Simulation of network B with current parameters
-        [acc(i, j), decision_reaction_times, error_reaction_times, decision_trial_indices, error_trial_indices, non_decision_trial_indices, avg_decision_rts(i, j), avg_error_rts(i, j), x2_thrr{i, j}, x17_thrr{i, j}, x2_three{i, j}, x17_three{i, j}, stc_rtc(i, j), std_error_rts(i, j), oo1_e1, oo2_e1] = fsm_A(num_trials, fs, decision_boundary, a_values(i), 0.1, 12, phi(j));
-    end
+
+for i=1:length(a_values1)
+
+% Simulation of network A
+[acc(i),decision_reaction_times,error_reaction_times,decision_trial_indices,error_trial_indices,non_decision_trial_indices{i},avg_decision_rts(i),avg_error_rts(i),x2_thr{i},x17_thr{i},x2_thre{i},x17_thre{i},stc_rtc(i),std_error_rts(i), o1_e1,o2_e1,input_1,input_2]=net_AS(num_trials,fs,decision_boundary,a_values1(i),0.07,12,phi,a_values2(i));
+
 end
 
 % End of simulation script for network A
